845a7771ed82adf2a0b16b024defeb15bd392067
olcolcolc <aleksandra@czyrnek.net>
2023-10-24 00:23:02 +0200

    feat(button): Add Button.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/navbar/Navbar";
+import Button from "./components/button/Button";
+
 
 function App() {
   return (
     <>
       <Navbar />
+      <Button/>
     </>
   );
 }
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const Button: React.FC = () => {
+      return (
+      <>
+      button
+      </>
+    );
+  };
+
+export default Button;
+
+  
