8b441294ddb0c2586b52263f850673c2b15cb50a
Ola Czyrnek <aleksandra@czyrnek.net>
2024-03-06 20:55:09 +0100

    refactor(style-updates): Change sprinkles animation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,12 +8,11 @@ const NavbarContainer = styled.div`
   margin: 0;
   display: flex;
   width: 100%;
-  background-color: inherit;
+  background-color: transparent;
   flex-direction: row;
   align-items: center;
   height: 100px;
   justify-content: start;
-
   ${theme.mixin.forMinWidth450(`
     justify-content: center;
   `)}
