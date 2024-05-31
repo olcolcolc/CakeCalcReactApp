4e629e6b93ce909089414a99d8672fc38e7f0f0d
Ola Czyrnek <aleksandra@czyrnek.net>
2024-05-31 21:36:30 +0200

    refactor(Annotation): Improve Annotation code
    - Refactor the Annotation component to move the ternary operator out of the JSX and into a separate constant for cleaner code.
    - Convert pixel values to rem units in the AnnotationDiv styled component for better scalability and responsiveness.
    - Use template literals to construct the data-testid attribute, reducing redundancy.

--- src/components/annotation/Annotation.tsx
+++ src/components/annotation/Annotation.tsx
@@ -2,33 +2,36 @@ import styled from "@emotion/styled";
 import { useTranslation } from "react-i18next";
 import { theme } from "../../styles/theme";
 
+// Define the type for the Annotation component props
 type AnnotationProps = {
   step: "0" | "1";
 };
 
+// Styled component for the annotation div
 const AnnotationDiv = styled.div`
   position: absolute;
-  top: 375px;
+  top: 23.5rem;
   font-size: ${theme.fontSize.annotation_mobile};
   text-align: center;
   font-family: "Regular_BasisGrotesqueArabicPro";
-  padding: 0 10px;
+  padding: 0 0.6rem;
   ${theme.mixin.forMinWidth650(`
     top: 460px;
     font-size: ${theme.fontSize.annotation_desktop};
-`)}
+  `)}
 `;
 
+// Annotation component
 const Annotation: React.FC<AnnotationProps> = ({ step }) => {
+  // Destructure the 't' function from useTranslation hook
   const { t } = useTranslation();
+
+  // Determine the annotation text based on the step prop
+  const annotationText = step === "0" ? "annotation.step0" : "annotation.step1";
 
   return (
     <AnnotationDiv>
-      {step === "0" ? (
-        <span data-testid="annotation-step0">{t("annotation.step0")}</span>
-      ) : (
-        <span data-testid="annotation-step1">{t("annotation.step1")}</span>
-      )}
+      <span data-testid={`annotation-step${step}`}>{t(annotationText)}</span>
     </AnnotationDiv>
   );
 };
